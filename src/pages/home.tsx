1e31b791a839a3c1e2f0bb6e6a1ae201506abd22
TacitusPie <kakmsaikou@gmail.com>
2022-09-19 21:15:14 +0800

    重构 chart1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,69 +2,24 @@ import React, {useEffect, useRef} from 'react';
 import './home.scss';
 import headBg from '../images/header.png';
 import * as  echarts from 'echarts';
+import {Chart1} from '../components/chart-1';
 
 const px = (n) => {
   return n / 2420 * (window as any).pageWidth;
 };
 
 export const Home = () => {
-  const divRef = useRef(null);
-
-  useEffect(() => {
-    const myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(12),
-        color: '#79839e'
-      },
-      xAxis: {
-        data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
-        axisTick: {show: false},
-        axisLabel: {
-          fontSize: px(12),
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split('');
-              array.splice(2, 0, '\n');
-              return array.join('');
-            }
-            return val;
-          }
-        },
-      },
-      grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40)
-      },
-      yAxis: {
-        splitLine: {show: false},
-        axisLine: {
-          lineStyle: {color: '#083'}
-        },
-        axisLabel: {
-          fontSize: px(12)
-        }
-      },
-      series: [{
-        type: 'bar',
-        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
-      }]
-    });
-  }, []);
 
   return (
     <div className="home">
       <header style={{backgroundImage: `url(${headBg})`}}/>
       <main>
         <section className="section1">
-          <div className="bordered 管辖统计">
-            <h2>案发派出所管辖统计</h2>
-            <div ref={divRef} className="chart"></div>
-          </div>
+          <Chart1/>
         </section>
-        <section className="bordered section2"></section>
+        <section className="bordered section2">
+
+        </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
