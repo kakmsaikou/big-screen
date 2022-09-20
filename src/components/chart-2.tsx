3200b8221713bbfa5bbdb673e3e7a5a2315f4612
TacitusPie <kakmsaikou@gmail.com>
2022-09-20 14:54:48 +0800

    添加渐变

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -6,39 +6,72 @@ import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart2 = () => {
   const divRef = useRef(null);
-
   useEffect(() => {
     const myChart = echarts.init(divRef.current);
     myChart.setOption(createEchartsOptions({
-      ...baseEchartOptions,
-      grid: {
-        x: px(100),
-        y: px(40),
-        x2: px(40),
-        y2: px(40)
-      },
-      xAxis: {
-        axisLabel: {
-          fontSize: px(12)
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01],
+          splitLine: {show: false},
+          axisLabel: {show: false}
         },
-        type: 'value',
-        boundaryGap: [0, 0.01]
+        yAxis: {
+          axisTick: {show: false},
+          type: 'category',
+          data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
+            '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+          axisLabel: {
+            formatter(val) {
+              return val.replace('公安局', '\n公安局');
+            }
+          }
+        },
+        series: [
+          {
+            name: '2011年',
+            type: 'bar',
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                  offset: 0,
+                  color: '#2034f9'
+                }, {
+                  offset: 1,
+                  color: '#04a1ff'
+                }])
+              }
+            }
+          },
+          {
+            name: '2012年',
+            type: 'bar',
+            data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                  offset: 0,
+                  color: '#b92ae8'
+                }, {
+                  offset: 1,
+                  color: '#6773e7'
+                }])
+              }
+            }
+          }
+        ]
       },
-      yAxis: {
-        type: 'category',
-        data: ['北京', '北京', '北京', '北京', '北京', '北京']
-      },
-      series: [{
-        type: 'bar',
-        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
-      }]
-    }));
+    ));
   }, []);
 
   return (
-    <div className="bordered 管辖统计">
+    <div className="bordered 破获排名">
       <h2>案件破获排名</h2>
-      <div ref={divRef} className="chart"></div>
+      <div ref={divRef} className="chart"/>
+      <div className="legend">
+        <span className="first"/> 破案排名1
+        <span className="second"/> 破案排名2
+      </div>
     </div>
   );
 };
