7bf32ec5dcc022043d5270cacb22e39e00304a0e
TacitusPie <kakmsaikou@gmail.com>
2022-10-07 17:00:53 +0800

    添加部署文件

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git checkout -b gh-pages &&
+git add . &&
+git commit -m 'update' &&
+git remote add origin git@github.com:kakmsaikou/big-screen.git &&
+git push -f -u origin "gh-pages" &&
+cd -
